9be3dd5660f6d0170942f08d56980f40f935c4e7
Blank Xiao <blankxiao0810@gmail.com>
2025-11-03 22:01:46 +0800

    feat: todoItem组件及数据类

---
+++ app/src/main/java/cn/szu/blankxiao/mycalendar/data/TodoItemData.kt
@@ -0,0 +1,10 @@
+package cn.szu.blankxiao.mycalendar.data
+
+import java.util.Date
+
+/**
+ * @author BlankXiao
+ * @description TodoItem
+ * @date 2025-11-03 21:36
+ */
+data class TodoItemData(val desc: String, val date: Date)
